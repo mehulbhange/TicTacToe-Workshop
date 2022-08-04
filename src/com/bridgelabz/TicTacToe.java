71348e63cf955b1c2832ee56b9affcbfc4fe245d
mehulbhange <bhangemehul@gmail.com>
2022-08-04 11:55:02 +0530

    MODIFIED:[Mehul] added playerInput method

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,7 +1,13 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     private static char[] board = new char[10];
+    private static char playerLetter;
+    private static char computerLetter;
+
+    static Scanner sc = new Scanner(System.in);
 
     public static void createBoard(){
         for(int i = 1; i < 10; i++){
@@ -9,9 +15,31 @@ public class TicTacToe {
         }
     }
 
+    /*
+    * UC-2 -> get userLetter from user
+    * */
+    public static void playerInput(){
+        System.out.println("Select the symbol (X/O) : ");
+        char tmp = sc.next().toUpperCase().charAt(0);
+        if( tmp == 'X' || tmp == 'O' ){
+            playerLetter = tmp;
+            System.out.println("Player symbol : "+ playerLetter);
+            if( playerLetter == 'X')
+                computerLetter = 'O';
+            else
+                computerLetter = 'X';
+        }else{
+            System.out.println("Invalid symbol. only choose X or O ");
+            playerInput();
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe game!");
 
         createBoard();
+
+        playerInput();
+
     }
 }
