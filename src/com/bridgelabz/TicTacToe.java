7683afd9ba3d6ffd7f8a94b28484e27365edc75f
mehulbhange <bhangemehul@gmail.com>
2022-08-04 11:23:31 +0530

    MODIFIED:[Mehul] adding createBoard method

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,7 +1,17 @@
 package com.bridgelabz;
 
 public class TicTacToe {
+    private static char[] board = new char[10];
+
+    public static void createBoard(){
+        for(int i = 1; i < 10; i++){
+            board[i] = ' ';
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe game!");
+
+        createBoard();
     }
 }
